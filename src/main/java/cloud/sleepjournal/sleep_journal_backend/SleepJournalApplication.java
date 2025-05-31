fd2933c5bf498976d8d279e03bd3f7e513ca594a
Zhaoyu Qu <zhaoyuqu@Zhaoyus-MacBook-Pro.local>
2025-05-31 23:16:50 +1000

    intial template project

---
+++ src/main/java/cloud/sleepjournal/sleep_journal_backend/SleepJournalApplication.java
@@ -0,0 +1,13 @@
+package cloud.sleepjournal.sleep_journal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/sleepjournal/sleep_journal_backend/SleepJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.sleepjournal.sleep_journal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
